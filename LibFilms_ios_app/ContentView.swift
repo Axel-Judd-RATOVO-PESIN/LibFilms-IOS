6907ff1d479f33676e9d17a8f4c8af764c3babce
Axel Judd Ratovo Pesin <axel-judd.ratovo-pesin@universite-paris-saclay.fr>
2025-11-05 10:14:52 +0100

    Initial Commit

---
+++ LibFilms_ios_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LibFilms_ios_app
+//
+//  Created by Axel .RP on 05/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LibFilms_ios_app/LibFilms_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibFilms_ios_appApp.swift
+//  LibFilms_ios_app
+//
+//  Created by Axel .RP on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LibFilms_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
